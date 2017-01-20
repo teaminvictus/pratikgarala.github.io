2f18faf7edc231d3b9aa223ba8e2038d2f4aeb0b
Pratik Garala <pratikgarala1311@gmail.com>
2017-01-20 11:09:57 +1100

    ThatsMePRatik - Skills Updated

--- ThatsMePratik/src/js/reducers/reducer-skills.js
+++ ThatsMePratik/src/js/reducers/reducer-skills.js
@@ -2,33 +2,43 @@ export default function () {
     return [
         {
             id: 1,
-            name: "SAP ABAP",
-            level: 90,
+            name: "React ",
+            level: 75,
         },
         {
             id: 2,
-            name: "SAP UI5",
-            level: 65,
+            name: "Redux",
+            level: 70,
         },
         {
             id: 3,
+            name: "HTML5",
+            level: 80,
+        },
+        {
+            id: 4,
+            name: "CSS3 /LESS/ SCSS",
+            level: 70,
+        },
+        {
+            id: 5,
+            name: "Javascript / ES6",
+            level: 75,
+        },
+        {
+            id: 6,
             name: "Java",
             level: 90,
         },
         {
-            id: 4,
-            name: "HTML5",
-            level: 85,
+            id: 7,
+            name: "SAP ABAP",
+            level: 90,
         },
         {
-            id: 5,
-            name: "CSS3",
+            id: 8,
+            name: "SAP UI5",
             level: 70,
-        },
-        {
-            id: 6,
-            name: "React",
-            level: 65,
         }
     ]
 }
