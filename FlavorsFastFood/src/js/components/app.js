3eaf8786f045b3b929902ebb99fa098edce79407
Pratik Garala <pratikgarala1311@gmail.com>
2017-01-29 17:23:30 +1100

    FlavorsFastFood - Menu Updated

--- FlavorsFastFood/src/js/components/app.js
+++ FlavorsFastFood/src/js/components/app.js
@@ -11,6 +11,7 @@ import WeeklySpecials from '../components/weeklySpecials';
 import Contact from '../components/contact';
 import SocialContact from '../components/socialContact';
 import Footer from '../components/footer';
+import MenuModal from '../container/menu-Modal';
 
 
 require("../../css/normalize.css");
@@ -22,12 +23,8 @@ require("../../css/font-awesome.min.css");
 require("../../css/picto-foundry-food.css");
 require("../../css/font-awesome.min.css");
 require("../../css/googlefontapi.css");
-
-
 require("../js/jquery.mixitup.min");
 require("../js/main");
-
-
 
 
 
@@ -41,43 +38,9 @@ const App = () => (
         <Contact/>
         <SocialContact/>
         <Footer/>
+        <MenuModal/>
 
 
-
-
-
-        <div className="portfolio-modal modal fade" id="masalapav"  role="dialog" aria-hidden="true">
-            <div className="modal-content">
-                <div className="close-modal" data-dismiss="modal">
-                    <div className="lr">
-                        <div className="rl">
-                        </div>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8 col-lg-offset-2">
-                            <div className="modal-body">
-                                <h2>Masala Pav</h2>
-                                <hr className="star-primary" />
-                                <img src={require("../../images/menu/masalapav.jpg")} className="img-responsive img-centered" alt="" />
-                                <p>
-                                    <h4>Ingredients</h4>
-                                    <br/>
-                                    onion, tomatoes, capsicum, garlic, ginger, green chilies
-                                    cumin seeds, turmeric powder, red chili powder, pav bhaji masala, butter, water
-                                    salt, pav, butter, lemon, coriander leave
-                                </p>
-                                <br/>
-                                <button type="button" className="btn btn-default" data-dismiss="modal">
-                                    <i className="fa fa-times"></i> Close
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 );
 
--- FlavorsFastFood/src/js/components/pricing.js
+++ FlavorsFastFood/src/js/components/pricing.js
@@ -2,6 +2,7 @@
  * Created by pratikgarala on 28/1/17.
  */
 import React from 'react';
+import MenuData from '../container/menu-data';
 
 const Pricing = () => (
     <section id ="pricing" className="description_content">
@@ -18,67 +19,7 @@ const Pricing = () => (
                             <li className="filter" data-filter="special">Special</li>
                             <li className="filter" data-filter="desert">Desert</li>
                         </ul>
-                        <ul id="portfolio">
-                            <li className="item regular">
-
-                                <a href="#masalapav" className="portfolio-link" data-toggle="modal" data-target="#masalapav">
-                                    <div className="caption">
-                                        <div className="caption-content">
-                                            <i className="fa fa-search-plus fa-3x"></i>
-                                        </div>
-                                    </div>
-                                    <img src={require("../../images/menu/masalapav.jpg")} className="img-responsive" alt="Food" />
-
-                                </a>
-                                <h2 className="white">$9.99</h2>
-
-                            </li>
-                            <li className="item dinner special">
-                                <a href="#" className="portfolio-link" data-toggle="modal">
-                                    <div className="caption">
-                                        <div className="caption-content">
-                                            <i className="fa fa-search-plus fa-3x"></i>
-                                        </div>
-                                    </div>
-                                    <img src={require("../../images/menu/pavbhaji.jpg")} className="img-responsive" alt="Food" />
-                                </a>
-
-                                <h2 className="white">$8.99</h2>
-                            </li>
-                            <li className="item dinner regular">
-                                <a href="#" className="portfolio-link" data-toggle="modal">
-                                    <div className="caption">
-                                        <div className="caption-content">
-                                            <i className="fa fa-search-plus fa-3x"></i>
-                                        </div>
-                                    </div>
-                                    <img src={require("../../images/menu/cheese_pavbhaji.jpg")} className="img-responsive" alt="Food" />
-                                </a>
-                                <h2 className="white">$9.99</h2>
-                            </li>
-                            <li className="item special">
-                                <a href="#" className="portfolio-link" data-toggle="modal">
-                                    <div className="caption">
-                                        <div className="caption-content">
-                                            <i className="fa fa-search-plus fa-3x"></i>
-                                        </div>
-                                    </div>
-                                    <img src={require("../../images/menu/tava_pulav.jpg")} className="img-responsive" alt="Food" />
-                                </a>
-                                <h2 className="white">$8.99</h2>
-                            </li>
-                            <li className="item dinner">
-                                <a href="#" className="portfolio-link" data-toggle="modal">
-                                    <div className="caption">
-                                        <div className="caption-content">
-                                            <i className="fa fa-search-plus fa-3x"></i>
-                                        </div>
-                                    </div>
-                                    <img src={require("../../images/menu/cheese_tava_pulav.jpg")} className="img-responsive" alt="Food" />
-                                </a>
-                                <h2 className="white">$9.99</h2>
-                            </li>
-                        </ul>
+                        <MenuData/>
                     </div>
                 </div>
             </div>
